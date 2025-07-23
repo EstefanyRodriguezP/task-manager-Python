c5eba19806e6033cd469c15d25b5bc40854100c3
Estefany Rodríguez <nicol.na14@gmail.com>
2025-07-23 17:19:19 -0400

    Se actualiza archivo main.py 23/07/25

--- main.py
+++ main.py
@@ -1,68 +1,44 @@
-""" 
-Objetivo
-En esta actividad, desarrollarás una aplicación de línea de comandos que permitirá gestionar tareas de manera sencilla. Con este proyecto, aplicarás conocimientos fundamentales de Python, incluyendo el uso de estructuras de datos, funciones, control de flujo y módulos.
-
-Contexto
-Imagina que eres un desarrollador que necesita una herramienta ligera para organizar sus pendientes diarios desde la terminal. No quieres depender de aplicaciones externas ni de herramientas visuales avanzadas, sino algo simple y funcional que puedas ejecutar en cualquier sistema con Python instalado.
-Para ello, construirás una aplicación que permita al usuario agregar tareas, verlas, marcarlas como completadas y eliminarlas. Además, las tareas deberán guardarse en un archivo para que no se pierdan cuando se cierre el programa.
-
-Requisitos del Proyecto
-1. Menú interactivo
-- La aplicación debe mostrar un menú en la consola con opciones numéricas para que el usuario pueda elegir qué acción realizar.
-
-2. Operaciones básicas:
-- Agregar una nueva tarea.
-- Listar todas las tareas con un indicador de estado (Pendiente o Completada).
-- Marcar una tarea como completada.
-- Eliminar una tarea.
-- Salir del programa.
-
-3. Estructuras de datos
-- Utilizar diccionarios para representar cada tarea.
-- Usar una lista para almacenar todas las tareas.
-
-4. Funciones
-- Dividir el código en funciones reutilizables para cada operación.
-
-5. Validaciones y manejo de errores
-- Evitar errores al ingresar opciones no válidas.
-- Manejar archivos de manera segura para evitar pérdidas de datos.
-
-Ejemplo de Uso
-Cuando el usuario ejecute el programa, verá un menú como el siguiente:
---- Gestor de Tareas ---
-1. Agregar tarea
-2. Ver tareas
-3. Marcar tarea como completada
-4. Eliminar tarea
-5. Salir
-Elige una opción: 
-Al seleccionar una opción se ejecuta el código detrás de esa opción volvemos al menu hasta que el usuario seleccione Salir. """
-
-tareas = []
+tareas = {}
+estados = ['Pendiente', 'Completada']
 
 def agregar_tarea():
-    return
-agregar_tarea()
+    while True:
+                descripcion = input('A continuación, ingresa la descripción de la tarea: ').strip().upper()
+                if descripcion != "":
+                    break
+                else:
+                    print("La descripción de la tarea no puede estar vacía. Por favor, intenta otra vez...")
+    estado_inicial = estados[0]
+    nueva_clave = 1
+    while nueva_clave in tareas:
+        nueva_clave += 1
+    tareas[nueva_clave] = {'descripcion': descripcion, 'estado': estado_inicial}
+    return f'La tarea "{descripcion}" se agregó correctamente con estado "{estado_inicial}"!'
 
 def ver_tareas():
-    return
-ver_tareas()
+    print('A continuación, se muestran todas las tareas registradas: ')
+    for clave, tarea in tareas.items():
+        print(f'{clave}. Descripción: {tarea["descripcion"]} - Estado: {tarea["estado"]}')
 
 def marcar_tarea_completa():
-    return
-marcar_tarea_completa()
+    tarea_completada = int(input('Ingresa el número de la tarea completada: '))
+    if tarea_completada in tareas:
+        tareas[tarea_completada]['estado'] = estados[1]
+        return f'Se modificó el estado de la tarea "{tareas[tarea_completada]['descripcion']}" a "{estados[1]}"!'
+    else:
+        return "La tarea no existe"
 
 def eliminar_tarea():
-    return
-eliminar_tarea()
-
+    tarea_eliminada = int(input('Ingresa el número de la tarea que quieres eliminar: '))
+    if tarea_eliminada in tareas:
+        tareas.pop(tarea_eliminada)
+        print('Se ha eliminado la tarea exitosamente!')
 
 def menu():
     print('Bienvenido al sistema!')
 
     while True:
-        print('''--- Gestor de Tareas ---
+        print('''\n--- Gestor de Tareas ---
           1. Agregar tarea
           2. Ver tareas
           3. Marcar tarea como completada
@@ -71,11 +47,11 @@ def menu():
         eleccion = input('Ingresa el número de tu opción: ')
         match eleccion:
             case '1':
-                agregar_tarea()
+                print(agregar_tarea())
             case '2':
                 ver_tareas()
             case '3':
-                marcar_tarea_completa()
+                print(marcar_tarea_completa())
             case '4':
                 eliminar_tarea()
             case '5':
@@ -85,13 +61,3 @@ def menu():
                 print('Opción no válida, intente nuevamente')
 
 menu()
-
-
-# abrir el archivo:
-# si archivo "tareas.txt" existe se abre, si no existe se crea
-""" with open('tareas.txt', w) as f:
-    f.write('Hola, eliminé el contenido anterior')
-
-# abrir y leer el archivo después de sobreescribirlo
-with open('tareas.txt', w) as f:
-    print(f.read()) """
